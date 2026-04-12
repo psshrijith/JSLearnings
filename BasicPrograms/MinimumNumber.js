9b82e32b7d730eb83b02820160b35e6a2acc0190
psshrijith <p.shrijith@talview.com>
2026-04-12 13:31:58 +0530

    minimum number in an array

--- BasicPrograms/MinimumNumber.js
+++ BasicPrograms/MinimumNumber.js
@@ -1,10 +1,13 @@
 const array = [0,5,11,1,3,2]
 let result = array[0];
 
-for (let i=1;i<array.length;i++){
-    if(array[i]<result){
-        result = array[i];
+function minimumNumber(array) {
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < result) {
+            result = array[i];
+        }
     }
+    return result;
 }
 
-console.log(result);
+console.log(minimumNumber(array));
