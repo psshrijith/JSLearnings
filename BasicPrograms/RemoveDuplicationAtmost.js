f3fc0340844418681a1fe881e17b8b4a4b8e9c75
psshrijith <p.shrijith@talview.com>
2026-04-19 12:21:05 +0530

    remove duplicates

---
+++ BasicPrograms/RemoveDuplicationAtmost.js
@@ -0,0 +1,17 @@
+function removeDuplicatesAtmostK(nums) {
+    if (nums.length === 0) return 0;
+
+    let i = 2;
+
+    for (let j = 2; j < nums.length; j++) {
+        if (nums[j] !== nums[i-2]) {
+            i++;
+            nums[i] = nums[j];
+        }
+
+    }
+
+    return i;
+}
+
+console.log(removeDuplicatesAtmostK([1, 1, 1,2,2,3]))
