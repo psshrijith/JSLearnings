abe1ef9cdae0797833696d975827ffd504b26fa6
psshrijith <p.shrijith@talview.com>
2026-04-23 14:47:32 +0530

    remove duplicates from array

---
+++ 03-algorithms/two-pointers/removeDuplicates.js
@@ -0,0 +1,13 @@
+function removeDuplicates(arr){
+    let i = 0;
+    for(let j=1;j<arr.length;j++){
+        if(arr[j] !== arr[i]){
+            i++;
+            arr[i] = arr[j];
+
+        }
+    }
+    return i+1;
+}
+
+console.log(removeDuplicates([1,1,2]))
