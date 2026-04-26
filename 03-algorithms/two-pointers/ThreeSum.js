dbe014c89b104dc7f0d522ce7169a34a5c67789c
psshrijith <p.shrijith@talview.com>
2026-04-26 18:38:08 +0530

    feat(jslearnings): three sum problem

--- 03-algorithms/two-pointers/ThreeSum.js
+++ 03-algorithms/two-pointers/ThreeSum.js
@@ -1,14 +1,26 @@
 function ThreeSum(nums){
     nums.sort((a,b) => a - b)
+    const result = []
     for(let i = 0; i < nums.length; i++){
-        for(let j=i+1;j<nums.length;j++){
-            for(let k=j+1;k<nums.length;k++){
-                if(nums[i]+nums[j]+nums[k] === 0){
-                    console.log(nums[i],nums[j],nums[k])
-                }
+        if(i > 0 && nums[i] === nums[i - 1]) continue
+        let left = i + 1
+        let right = nums.length - 1
+        while(left < right){
+            const sum = nums[i] + nums[left] + nums[right]
+            if(sum === 0){
+                result.push([nums[i], nums[left], nums[right]]);
+                left++;
+                right--;
             }
-        }
+            else if(sum < 0){
+                left++;
+            }
+            else{
+                right--;
+            }         
     }
 }
+    return result;
+}
 
-ThreeSum([-1,0,1,2,-1,-4])
+console.log(ThreeSum([-1,0,1,2,-1,-4]));
