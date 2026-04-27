8e24afb90a6351bc160c9af1bbcaced37c7d3b08
psshrijith <p.shrijith@talview.com>
2026-04-27 06:45:52 +0530

    feat(jslearnings): longest substring without repeating characters

--- 03-algorithms/two-pointers/LongestSubstring.js
+++ 03-algorithms/two-pointers/LongestSubstring.js
@@ -1,15 +1,18 @@
-let a = "dvdf";
+let string = "dvdf";
 let set = new Set();
 let maxLength = 0;
 let left = 0;
 
-for(let i=0;i<a.length;i++){
-    while(set.has(a[i])){
-        set.delete(a[left]);
-        left++;
+function LongestSubstring(s){
+    for(let i=0;i<a.length;i++){
+        if(set.has(a[i])){
+            set.delete(a[left]);
+            left++;
+        }
+        set.add(a[i]);
+        maxLength = Math.max(maxLength, set.size);
     }
-    set.add(a[i]);
-    maxLength = Math.max(maxLength, set.size);
+    return maxLength;
 }
 
-console.log(maxLength);
+console.log(LongestSubstring(string));
