1773d5f05b1c3a96c4136fcd64607bc7f5cb68e6
P S Shrijith <shrijithps89@gmail.com>
2026-04-29 07:45:47 +0530

    Update ThreeSum.js

--- 03-algorithms/two-pointers/ThreeSum.js
+++ 03-algorithms/two-pointers/ThreeSum.js
@@ -3,10 +3,10 @@ function ThreeSum(nums){
     const result = []
     for(let i = 0; i < nums.length; i++){
         if(i > 0 && nums[i] === nums[i - 1]) continue;
-        let left = i + 1
-        let right = nums.length - 1
+        let left = i + 1;
+        let right = nums.length - 1;
         while(left < right){
-            const sum = nums[i] + nums[left] + nums[right]
+            const sum = nums[i] + nums[left] + nums[right];
             if(sum === 0){
                 result.push([nums[i], nums[left], nums[right]]);
                 left++;
