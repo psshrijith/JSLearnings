350a0dcc3ccd8400190787534a62ce6bd8fc83ec
psshrijith <p.shrijith@talview.com>
2026-05-03 22:43:53 +0530

    feat(jsLearnings): recursive binary search

---
+++ 03-algorithms/recursion/recursiveBinarySearch.js
@@ -0,0 +1,16 @@
+function recursiveBinarySearch(array,target, left=0, right=arr.length-1){
+    if(left > right) {
+        return -1
+    }
+    let mid = Math.floor((left + right) / 2);
+    if (array[mid] === target) {
+        return mid;
+    }
+    if (target < array[mid]) {
+        return recursiveBinarySearch(array, target, left, mid - 1)
+    }
+    return recursiveBinarySearch(array, target, mid + 1, right);
+}
+
+const arr = [1, 3, 5, 7, 9, 11];
+console.log(recursiveBinarySearch(arr, 7)); // 3
