d5822b72ac93f1e78a47a045ec36135946924f74
P S Shrijith <shrijithps89@gmail.com>
2026-05-04 09:15:45 +0530

    Refactor ThreeSum function for clarity and consistency
    Refactored sorting. Improved readability by ensuring proper formatting and structure.

--- 03-algorithms/two-pointers/three-sum.js
+++ 03-algorithms/two-pointers/three-sum.js
@@ -1,12 +1,14 @@
 function ThreeSum(nums){
-    nums.sort((a,b) => a - b)
+    nums.sort((a,b) => a - b);
     const result = []
     for(let i = 0; i < nums.length; i++){
-        //if(i > 0 && nums[i] === nums[i - 1]) continue
-        let left = i + 1
-        let right = nums.length - 1
+        if(i > 0 && nums[i] === nums[i - 1]){
+            continue;
+        }
+        let left = i + 1;
+        let right = nums.length - 1;
         while(left < right){
-            const sum = nums[i] + nums[left] + nums[right]
+            const sum = nums[i] + nums[left] + nums[right];
             if(sum === 0){
                 result.push([nums[i], nums[left], nums[right]]);
                 left++;
