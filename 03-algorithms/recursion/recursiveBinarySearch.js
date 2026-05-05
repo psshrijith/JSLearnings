512b2f59ed73141b9c919f109e1eefc463e97115
P S Shrijith <shrijithps89@gmail.com>
2026-05-05 21:45:12 +0530

    Fix syntax error in recursiveBinarySearch function

--- 03-algorithms/recursion/recursiveBinarySearch.js
+++ 03-algorithms/recursion/recursiveBinarySearch.js
@@ -1,6 +1,6 @@
 function recursiveBinarySearch(array,target, left=0, right=arr.length-1){
     if(left > right) {
-        return -1
+        return -1;
     }
     let mid = Math.floor((left + right) / 2);
     if (array[mid] === target) {
