84e85c42280bf14fd8d41579a57ef8cfe6e23b69
psshrijith <p.shrijith@talview.com>
2026-05-31 09:45:24 +0530

    feat(jsLearnings): frequency counter

--- 03-algorithms/frequency-counter/squares.js
+++ 03-algorithms/frequency-counter/squares.js
@@ -1,31 +1,28 @@
 function squares(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
 
-    if (arr1.length !== arr2.length) {
-        return false;
+  const frequencyCounter1 = {};
+  const frequencyCounter2 = {};
+
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+
+  for(let key in frequencyCounter1) {
+    if(!(key**2 in frequencyCounter2)) {
+      return false;
     }
 
-    let frequencyCounter1 = {};
-    let frequencyCounter2 = {};
-
-    for(let val of arr1) {
-        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+    if(frequencyCounter2[key**2] !== frequencyCounter1[key]) {
+      return false;
     }
+  }
 
-    for(let val of arr2){
-        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
-    }
-
-    for(let key in frequencyCounter1) {
-        if(!(key ** 2 in frequencyCounter2)) {
-            return false;
-        }
-
-        if(frequencyCounter1[key] !== frequencyCounter2[key ** 2]) {
-            return false;
-        }
-    }
-    
-    return true;
+  return true;
 }
-
-console.log(squares([1,2,3], [4,1,9]));
