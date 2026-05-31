8cbc230bc1d797da429fee01a7787701e5cdabaa
psshrijith <shrijithps89@gmail.com>
2026-05-31 14:03:07 +0530

    feat(jslearnings): count of unique numbers in a array

---
+++ 03-algorithms/sliding-window/count-unique.js
@@ -0,0 +1,19 @@
+function countUnique(arr) {
+    let left = 0;
+    let right = left+1;
+    let unique = 1;
+    while(right < arr.length){
+        if(arr[left] !== arr[right]){
+            unique++;
+            left++;
+            right++;
+        }
+        else if(arr[left] === arr[right]){
+            right++;
+        }
+
+    }
+    return unique;
+}
+
+console.log(countUnique([1, 2, 3, 4, 4, 4, 7,7, 12, 12, 13, 14, 15]))
