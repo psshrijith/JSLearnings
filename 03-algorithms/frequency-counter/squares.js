851849be86ecaf3f02812b76a9e80f9027d566da
psshrijith <p.shrijith@talview.com>
2026-05-31 09:30:37 +0530

    feat(jsLearnings): frequency counter

---
+++ 03-algorithms/frequency-counter/squares.js
@@ -0,0 +1,31 @@
+function squares(arr1, arr2) {
+
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    let frequencyCounter1 = {};
+    let frequencyCounter2 = {};
+
+    for(let val of arr1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+    }
+
+    for(let val of arr2){
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+    }
+
+    for(let key in frequencyCounter1) {
+        if(!(key ** 2 in frequencyCounter2)) {
+            return false;
+        }
+
+        if(frequencyCounter1[key] !== frequencyCounter2[key ** 2]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+console.log(squares([1,2,3], [4,1,9]));
