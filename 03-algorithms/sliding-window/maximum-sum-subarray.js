f1401666c28d603d6b8cef3fddcf461e75a67230
psshrijith <shrijithps89@gmail.com>
2026-06-01 22:14:25 +0530

    feat(jslearnings): maximum sum subarray

--- 03-algorithms/sliding-window/maximum-sum-subarray.js
+++ 03-algorithms/sliding-window/maximum-sum-subarray.js
@@ -11,4 +11,24 @@ function maximumSumSubarray(arr, k) {
     return maxSum;
 }
 
-console.log(maximumSumSubarray([1,2,5,2,8,1,5], 4))
+console.log(maximumSumSubarray([1,2,5,2,8,1,5], 4));
+
+//Optimized Solution
+function maximumSumSubarray(arr, k) {
+    let sum = 0;
+    let windowSum = 0;
+    for(let i = 0; i < k; i++){
+        sum += arr[i];
+    }
+
+    windowSum = sum;
+
+    for(let i = k; i < arr.length; i++){
+        sum += arr[i] - arr[i-k];
+        windowSum = Math.max(windowSum, sum);
+    }
+    return windowSum;
+}
+
+console.log(maximumSumSubarray([1,2,5,2,8,1,5], 4));
+
