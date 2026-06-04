9ab2f4638dc0a9ce2dafac224036c91dde1ddb85
psshrijith <shrijithps89@gmail.com>
2026-06-04 07:45:38 +0530

    feat(jsLearnings): isSubsequence

---
+++ 03-algorithms/two-pointers/is-subsequence.js
@@ -0,0 +1,18 @@
+function isSubsequence(message, string) {
+    let left = 0;
+    let result = "";
+    for(let j of string){
+        if(j === message[left]){
+            result += j;
+            left++;
+        }
+
+
+    }
+    return result===message;
+}
+
+console.log(isSubsequence("hello", "hello world"));
+console.log(isSubsequence("sing", "sting"));
+console.log(isSubsequence("abc", "abracadabra"));
+console.log(isSubsequence("abc", "acb"));
