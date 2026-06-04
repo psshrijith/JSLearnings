3453203494964ca44bca87791e8e92c1c1a1c898
psshrijith <shrijithps89@gmail.com>
2026-06-04 07:14:20 +0530

    feat(jsLearnings): findDuplicates

--- 03-algorithms/frequency-counter/findAllDuplicates.js
+++ 03-algorithms/frequency-counter/findAllDuplicates.js
@@ -8,7 +8,26 @@ function FindAllDuplicates(nums) {
 
     for(let num in frequencyCounter){
         if(frequencyCounter[num] > 1){
+            duplicates.push(Number(num));
+        }
+    }
+    return duplicates;
+}
+
+console.log(FindAllDuplicates([4,3,2,7,8,2,3,1]));
+
+//Solution 2
+
+function FindAllDuplicates1(nums) {
+    const duplicates = [];
+
+    const set = new Set();
+    
+    for(let num of nums){
+        if(set.has(num)){
             duplicates.push(num);
+        } else {
+            set.add(num);
         }
     }
     return duplicates;
