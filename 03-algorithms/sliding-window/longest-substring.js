a9eee601bc2ed8ebb43e0a5683052cfefe54e6e5
psshrijith <shrijithps89@gmail.com>
2026-06-08 21:29:14 +0530

    feat(jsLearnings): longest substring

---
+++ 03-algorithms/sliding-window/longest-substring.js
@@ -0,0 +1,22 @@
+function longestSubstring(s) {
+  let longest = 0;
+  let left = 0;
+  let seen = {};
+
+  for(let right=0;right<s.length;right++) {
+    const char = s[right];
+
+    if(seen[char] !== undefined) {
+      left = Math.max(left, seen[char] + 1);
+    }
+
+    seen[char] = right;
+    longest = Math.max(longest, right - left + 1);
+  }
+
+  return longest;
+}
+
+console.log(longestSubstring("abcabcbb")); // 3
+console.log(longestSubstring("bbbbb")); // 1
+console.log(longestSubstring("pwwkew")); // 3
