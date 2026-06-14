808608278192555dbbade99073405e0dc93137e8
psshrijith <shrijithps89@gmail.com>
2026-06-14 15:36:44 +0530

    feat(jsLearnings): find the product of a numbers using recursion

--- 03-algorithms/recursion/product.js
+++ 03-algorithms/recursion/product.js
@@ -1,8 +1,9 @@
-function product(arr) {
+function productOfArray(arr) {
     if (arr.length === 0) {
         return 1;
     }
-    return arr[0] * product(arr.slice(1));
+    return arr[0] * productOfArray(arr.slice(1));
 }
 
-console.log(product([1, 2, 3, 4])); // Output: 24
+
+console.log(productOfArray([1, 2, 3, 4])); // Output: 24
