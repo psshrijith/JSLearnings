cd509db90cd48d2936c8ba97bf752915135d0673
psshrijith <shrijithps89@gmail.com>
2026-06-14 20:36:22 +0530

    feat(jsLearnings): isPalindrome

---
+++ 03-algorithms/recursion/flattenArray.js
@@ -0,0 +1,18 @@
+function flattenArray(arr) {
+    let result = [];
+
+    console.log("array", arr)
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            console.log(arr);
+            result = result.concat(flattenArray(arr[i]));
+        } else {
+            result.push(arr[i]);
+            console.log("in bte secod loop")
+        }
+    }
+
+    return result;
+}
+
+console.log(flattenArray([[1,2,3,4],[5,7,7,33]]));
