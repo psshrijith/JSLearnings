12f46c2e9f3f6c92241042cf03f0779b0bcabf04
psshrijith <shrijithps89@gmail.com>
2026-06-14 08:52:30 +0530

    feat(jsLearnings): divide and conquer - sorted frequency

--- 03-algorithms/divide-and-conquer/sorted-frequency.js
+++ 03-algorithms/divide-and-conquer/sorted-frequency.js
@@ -49,6 +49,5 @@ function sortedFrequency(arr, num) {
     return lastOccurrence - firstOccurrence + 1;
 }
 
-console.log(sortedFrequency([1,1,2,2,2,3], 2)); // 3
-console.log(sortedFrequency([1,1,2,2,2,3], 3)); // 1
-console.log(sortedFrequency([1,1,2,2,2,3], 4)); // -1
+console.log(sortedFrequency([3,4,1,2], 4));
+console.log(sortedFrequency([6,7,8,9,1,2,3,4], 8));
