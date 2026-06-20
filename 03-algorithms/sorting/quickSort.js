ad19543ead66b0e2895f0ac7997668aabeddffac
psshrijith <shrijithps89@gmail.com>
2026-06-20 19:48:07 +0530

    feat(jsLearnings): quick sort

--- 03-algorithms/sorting/quickSort.js
+++ 03-algorithms/sorting/quickSort.js
@@ -1,24 +1,33 @@
-function sorter(arr){
-    let i=-1;
-    let j=0;
-    let pivot = arr[arr.length-1];
-    let temp = Infinity;
+function partition(arr, low, high) {
+    let pivot = arr[high];
+    let i = low - 1;
 
-    while(j< arr.length-1){
-        if(arr[j]<pivot){
+    for (let j = low; j < high; j++) {
+        if (arr[j] < pivot) {
             i++;
-            temp = arr[i];
+
+            let temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
         }
-        j++;
     }
-    let temp1 = arr[i+1];
-    arr[i+1] = arr[j];
-    arr[j] = temp1;
 
-    return arr;
+    let temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
 
+    return i + 1;
 }
 
-console.log(sorter([2,4,1,3]));
+function quickSort(arr, low, high){
+    if(low>=high) return;
+
+    let pivotIndex = partition(arr, low, high);
+
+    quickSort(arr, low, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, high);
+}
+
+let arr = [2, 4, 1, 3];
+quickSort(arr, 0, arr.length - 1);
+console.log(arr);
