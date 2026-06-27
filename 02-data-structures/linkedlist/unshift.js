b3d63b52753cb08b0475b9fac340faf3cca55542
psshrijith <shrijithps89@gmail.com>
2026-06-27 12:38:56 +0530

    feat(jsLearnings): implement unshift method for linkedlist

---
+++ 02-data-structures/linkedlist/unshift.js
@@ -0,0 +1,15 @@
+class SingleLinkedList{
+
+    unshift(value) {
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+}
